c842bf57d87fa8200d5be5a1aabf5478b909de12
Matheus <mgmazeto@gmail.com>
2020-09-12 15:18:43 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import GlobalStyles from 'styles/global'
+
+const App = () => {
+  return (
+    <>
+      <div>UI Clone Template</div>
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
